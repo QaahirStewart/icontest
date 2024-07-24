c6dc3140fcfc45c1de16abcf8568de7485909042
QRST <87553353+QaahirStewart@users.noreply.github.com>
2024-07-24 17:46:44 -0400

    Reproduction Ex

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <Icon name="fa6-solid:4" />
+        <Icon name="fa6-solid:1" />
+        <Icon name="fa6-solid:6" />
+    </div>
+</template>
+
+<style scoped></style>
